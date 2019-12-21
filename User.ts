5a8f3c1f2deea02425f3c8fd5f71e3b7dcec959d
VINXIS <oykxf2@gmail.com>
2019-12-21 00:18:03 -0700

    user model update

--- User.ts
+++ User.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, OneToOne, JoinColumn } from "typeorm";
 
 @Entity()
 export class User extends BaseEntity {
@@ -10,6 +10,57 @@ export class User extends BaseEntity {
     username: string;
 
     @Column()
-    isStaff: boolean;
+    avatar: string;
+
+    @OneToOne(type => Discord)
+    @JoinColumn()
+    discord: Discord;
+    
+    @OneToOne(type => Osu)
+    @JoinColumn()
+    osu: Osu;
+
+    @Column({ default: Date.now() })
+    registered: Date;
+    
+    @Column({ default: Date.now() })
+    lastLogin: Date;
+
+}
+
+@Entity()
+export class Discord {
+
+    @Column()
+    userID: string;
+
+    @Column()
+    accessToken: string;
+
+    @Column()
+    refreshToken: boolean;
+
+    @Column({ default: Date.now() })
+    dateAdded: Date;
+
+    @Column({ default: Date.now() })
+    lastVerified: Date;
+
+}
+
+@Entity()
+export class Osu {
+
+    @Column()
+    accessToken: string;
+
+    @Column()
+    refreshToken: boolean;
+
+    @Column({ default: Date.now() })
+    dateAdded: Date;
+
+    @Column({ default: Date.now() })
+    lastVerified: Date;
 
 }
